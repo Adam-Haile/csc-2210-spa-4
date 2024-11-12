2d69fe58180b03d618e6d3d24fa0d95cb5da796a
Aiden Miller <100258120+a-miller77@users.noreply.github.com>
2024-11-12 13:13:46 -0600

    can navigate correctly

--- src/Player.cpp
+++ src/Player.cpp
@@ -29,8 +29,7 @@ vector<string> Player::search(Map* map) {
         for (int dy = -1; dy <= 1; dy ++) {
             int Y = playerY + dy;
             bool playerTile = dx == 0 && dy == 0;
-            bool validTile = X >= 0 && X < 10 && Y >= 0 && Y < 8;
-            if (!playerTile && validTile) {
+            if (!playerTile && map->validRoom(X,Y)) {
                 vector<string> messages = map->getRoom(X, Y)->getAllMessages();
                 //add messages to result
                 result.insert(result.end(), messages.begin(), messages.end());
